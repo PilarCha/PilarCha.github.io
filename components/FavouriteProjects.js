20c6e2ca49876cf2540d3b7934f9afce07236390
Pilar Chavez <pchavez92@gmail.com>
2022-02-17 21:44:26 -0800

    Updated the Favorite projects with anchor tags to open in a new page

--- components/FavouriteProjects.js
+++ components/FavouriteProjects.js
@@ -7,7 +7,7 @@ export default function FavouriteProjects() {
       <div className="max-w-6xl mx-auto">
         <header className="flex flex-col md:flex-row justify-between items-center pt-40 mx-10 md:my-20 lg:my-0">
           <h1 className="text-6xl lg:text-9xl max-w-lg font-bold text-gray-500 my-20 md:my-0 md:text-white dark:text-gray-600 text-center">
-            Favourite Projects
+            Favorite Projects
           </h1>
           <Link href="/projects">
             <a className="mb-20 md:mb-0 px-8 py-4 rounded-md bg-white shadow-lg text-xl font-semibold flex flex-row space-x-4 items-center dark:text-gray-700">
@@ -36,6 +36,7 @@ export default function FavouriteProjects() {
           {/* Single card */}
           <a
             href="https://tailwindmasterkit.com"
+            target="_blank"
             className="w-full block col-span-3 shadow-2xl"
           >
             <div className="relative overflow-hidden">
@@ -45,7 +46,7 @@ export default function FavouriteProjects() {
                 className="transform hover:scale-125 transition duration-2000 ease-out"
               />
               <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
-                Tailwind Master Kit
+                Vue Pomodoro Timer
               </h1>
               <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
                 01
@@ -55,6 +56,7 @@ export default function FavouriteProjects() {
           {/* Single card */}
           <a
             href="https://placeholdertech.in"
+            target="_blank"
             className="w-full block col-span-3  sm:col-span-2 shadow-2xl"
           >
             <div className="relative overflow-hidden">
@@ -75,6 +77,7 @@ export default function FavouriteProjects() {
           {/* Single card */}
           <a
             href="https://manuarora.in"
+            target="_blank"
             className="w-full block col-span-3 sm:col-span-1  object-cover"
           >
             <div className="relative overflow-hidden shadow-2xl">
