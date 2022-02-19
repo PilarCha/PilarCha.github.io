80fd47237ee07b998c44570eb2027fe98a8f3410
Pilar Chavez <pchavez92@gmail.com>
2022-02-19 15:00:32 -0800

    updated the api call to github. Now it showcases the most recently updated projects in my github

--- components/FavouriteProjects.js
+++ components/FavouriteProjects.js
@@ -67,7 +67,7 @@ export default function FavouriteProjects() {
                 className="transform hover:scale-125 transition duration-2000 ease-out"
               />
               <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
-                PlaceholderTech
+                FaShare
               </h1>
               <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
                 02
